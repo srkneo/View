fc9d6ca36c61fb7b765b778eeba5bacb3ba7d4cf
srkneo <yadavmaheshing@gmail.com>
2016-01-30 15:14:29 +0530

    Max

---
+++ View1/Infrastructure/DebugDataViewEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View1.Infrastructure
+{
+    public class DebugDataViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindView(ControllerContext controllerContext, string ViewName, string masterName, bool useCache)
+        {
+            if(ViewName == "DebugData")
+            {
+                return new ViewEngineResult(new DebugDataView(), this);
+            }
+            else
+            {
+                return new ViewEngineResult(new string[] { "No view Debug " });
+            }
+        }
+
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string PartialViewName, bool useCache)
+        {
+            return new ViewEngineResult(new string[] { "No view Debug " });
+        }
+
+        public void ReleaseView (ControllerContext controllerContext, IView view)
+        {
+            //Do nothing
+        }
+    }
+}
